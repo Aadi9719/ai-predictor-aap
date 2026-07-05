12107bea19e021b86bcfe0b093600d543617d21a
Aadi9719 <checkmeet39@gmail.com>
2026-07-05 16:49:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ alert("script.js Connected Successfully ✅");
 
 document.getElementById("analyzeBtn").onclick = function(){
 
+    let n1 = Number(document.getElementById("n1").value);
+    let n2 = Number(document.getElementById("n2").value);
+
     document.getElementById("result").innerHTML =
-    "AI Started Successfully 🚀";
+
+    "Number 1 : " + n1 +
+    "<br><br>" +
+    "Number 2 : " + n2;
 
 };
