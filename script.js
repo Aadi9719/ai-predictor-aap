74480c472814b7dc5da92483f3821e4b6b8ce924
Aadi9719 <checkmeet39@gmail.com>
2026-07-05 16:55:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ alert("script.js Connected Successfully ✅");
 document.getElementById("analyzeBtn").onclick = function(){
 
     let n1 = Number(document.getElementById("n1").value);
-    let n2 = Number(document.getElementById("n2").value);
+let n2 = Number(document.getElementById("n2").value);
+let n3 = Number(document.getElementById("n3").value);
+let n4 = Number(document.getElementById("n4").value);
+let n5 = Number(document.getElementById("n5").value);
+
+let numbers = [n1,n2,n3,n4,n5];
 
     document.getElementById("result").innerHTML =
+numbers.join(" | ");
 
     "Number 1 : " + n1 +
     "<br><br>" +
