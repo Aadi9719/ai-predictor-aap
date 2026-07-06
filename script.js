542afb8cf6a71ab0d32cdf3fca084a6041d23000
Aadi9719 <checkmeet39@gmail.com>
2026-07-06 14:22:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 alert("Script Loaded");
 
 document.getElementById("analyzeBtn").onclick = function () {
-    alert("Analyze Button Working ✅");
+
+    let n1 = Number(document.getElementById("n1").value);
+    let n2 = Number(document.getElementById("n2").value);
+    let n3 = Number(document.getElementById("n3").value);
+    let n4 = Number(document.getElementById("n4").value);
+    let n5 = Number(document.getElementById("n5").value);
+
+    let numbers = [n1, n2, n3, n4, n5];
+
+    // Save history
+    allResults.push(...numbers);
+
+    localStorage.setItem(
+        "allResults",
+        JSON.stringify(allResults)
+    );
+
+    // Temporary prediction
+    nextPrediction = Math.floor(Math.random() * 10);
+
+    document.getElementById("result").innerHTML =
+    `
+    <h2>AI Prediction 🔥</h2>
+
+    Next Number :
+    <b>${nextPrediction}</b>
+
+    <br><br>
+
+    Total Saved Numbers :
+    ${allResults.length}
+    `;
+
+    console.log(allResults);
+
 };
 
 document.getElementById("checkBtn").onclick = function () {
