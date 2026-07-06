03bd1566a6bd3da022151e074aa2d5f67dc1b570
Aadi9719 <checkmeet39@gmail.com>
2026-07-06 13:37:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ document.getElementById("analyzeBtn").onclick = function () {
 document.getElementById("checkBtn").onclick = function () {
     alert("Check Button Working ✅");
 };
+// =========================
+// AI DATA
+// =========================
+
+let allResults =
+JSON.parse(localStorage.getItem("allResults")) || [];
+
+let patternMemory =
+JSON.parse(localStorage.getItem("patternMemory")) || {};
+
+let aiWins =
+Number(localStorage.getItem("aiWins")) || 0;
+
+let aiLosses =
+Number(localStorage.getItem("aiLosses")) || 0;
+
+let nextPrediction = null;
