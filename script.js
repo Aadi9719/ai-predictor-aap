6ee9af9e7d02150213fc670a3caddbe4194c4505
Aadi9719 <checkmeet39@gmail.com>
2026-07-06 12:23:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,9 @@
-alert("script.js Connected Successfully ✅");
+alert("Script Loaded");
 
-let allResults =
-JSON.parse(localStorage.getItem("allResults")) || [];
-
-document.getElementById("analyzeBtn").onclick = function(){
-
-    let n1 = Number(document.getElementById("n1").value);
-let n2 = Number(document.getElementById("n2").value);
-let n3 = Number(document.getElementById("n3").value);
-let n4 = Number(document.getElementById("n4").value);
-let n5 = Number(document.getElementById("n5").value);
-
-let numbers = [n1,n2,n3,n4,n5];
-
-    allResults.push(...numbers);
-
-    localStorage.setItem(
-"allResults",
-JSON.stringify(allResults)
-);
-
-console.log(allResults);
-    
-    document.getElementById("result").innerHTML =
-numbers.join(" | ");
-
-    "Number 1 : " + n1 +
-    "<br><br>" +
-    "Number 2 : " + n2;
-
+document.getElementById("analyzeBtn").onclick = function () {
+    alert("Analyze Button Working ✅");
 };
 
-document.getElementById("checkBtn").onclick = function(){
-
-    let actualResult = Number(
-        prompt("Enter Actual Result")
-    );
-
-    if(isNaN(actualResult)){
-        alert("Please Enter Valid Number");
-        return;
-    }
-
-    updateLearningMemory(actualResult);
-
-    alert("AI Learned Successfully ✅");
-
+document.getElementById("checkBtn").onclick = function () {
+    alert("Check Button Working ✅");
 };
-
-alert("END OF SCRIPT");
