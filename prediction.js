67833fb04044b57e6f91f15e66e2151fde7d4c84
Aadi9719 <checkmeet39@gmail.com>
2026-07-10 09:24:20 +0530

    Update prediction.js

--- prediction.js
+++ prediction.js
@@ -1,1 +1,23 @@
+function getColorPrediction(number){
 
+    if([1,3,7,9].includes(number)){
+        return "🟢 GREEN";
+    }
+
+    if([2,4,6,8].includes(number)){
+        return "🔴 RED";
+    }
+
+    return "🟣 VIOLET";
+
+}
+
+function getBigSmallPrediction(number){
+
+    if(number >= 5){
+        return "🔵 BIG";
+    }
+
+    return "🟡 SMALL";
+
+}
