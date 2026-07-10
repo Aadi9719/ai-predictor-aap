b276c5a1968264893d7e7551bd87164bb2c6d61a
Aadi9719 <checkmeet39@gmail.com>
2026-07-10 10:05:47 +0530

    Update prediction.js

--- prediction.js
+++ prediction.js
@@ -21,3 +21,41 @@ function getBigSmallPrediction(number){
     return "🟡 SMALL";
 
 }
+
+function getTrendPrediction(){
+
+    if(allResults.length < 20){
+        return null;
+    }
+
+    let recent = allResults.slice(-20);
+
+    let count = {};
+
+    recent.forEach(n => {
+
+        if(!count[n]){
+            count[n] = 0;
+        }
+
+        count[n]++;
+
+    });
+
+    let best = null;
+    let max = -1;
+
+    for(let num in count){
+
+        if(count[num] > max){
+
+            max = count[num];
+            best = Number(num);
+
+        }
+
+    }
+
+    return best;
+
+}
