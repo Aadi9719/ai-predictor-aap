abe689409d2bab8c048f606a8ce21da071c376d8
Aadi9719 <checkmeet39@gmail.com>
2026-07-10 09:18:45 +0530

    Update pattern.js

--- pattern.js
+++ pattern.js
@@ -1,1 +1,63 @@
+function getPatternPrediction(){
 
+    let pattern = allResults.slice(-6).join(",");
+
+    let searchPatterns = [
+
+        allResults.slice(-6).join(","),
+
+        allResults.slice(-5).join(","),
+
+        allResults.slice(-4).join(","),
+
+        allResults.slice(-3).join(",")
+
+    ];
+
+    let foundPattern = null;
+
+    for(let p of searchPatterns){
+
+        if(patternMemory[p]){
+
+            foundPattern = p;
+
+            break;
+
+        }
+
+    }
+
+    if(foundPattern === null){
+
+        return null;
+
+    }
+
+    pattern = foundPattern;
+
+    let nextNumbers = patternMemory[pattern].nextNumbers;
+
+    if(!nextNumbers){
+
+        return null;
+
+    }
+
+    let bestNumber = null;
+    let maxCount = -1;
+
+    for(let num in nextNumbers){
+
+        if(nextNumbers[num] > maxCount){
+
+            maxCount = nextNumbers[num];
+            bestNumber = Number(num);
+
+        }
+
+    }
+
+    return bestNumber;
+
+}
