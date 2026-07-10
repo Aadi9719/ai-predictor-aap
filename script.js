14aa5e44efa4e1e14658fe7ffe58c80fc78615b8
Aadi9719 <checkmeet39@gmail.com>
2026-07-10 09:32:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -293,46 +293,6 @@ function getPredictionConfidence(){
 
 }
 
-function updateStats(){
-
-    document.getElementById("totalPatterns").innerText =
-    Object.keys(patternMemory).length;
-
-    document.getElementById("wins").innerText =
-    aiWins;
-
-    document.getElementById("losses").innerText =
-    aiLosses;
-
-    let total = aiWins + aiLosses;
-
-    let accuracy = 0;
-
-    if(total > 0){
-
-        accuracy = Math.round((aiWins / total) * 100);
-
-    }
-
-    document.getElementById("accuracy").innerText =
-    accuracy + "%";
-
-}
-
-function addHistory(text){
-
-    let historyList =
-    document.getElementById("historyList");
-
-    let item =
-    document.createElement("p");
-
-    item.innerHTML = text;
-
-    historyList.prepend(item);
-
-}
-
 // =========================
 // AI DATA
 // =========================
