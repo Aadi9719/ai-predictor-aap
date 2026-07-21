8d9d59fd2babea2a294457d21c9b76a0ec70613d
Aadi9719 <checkmeet39@gmail.com>
2026-07-21 17:50:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -222,7 +222,7 @@ localStorage.setItem(
 
     updateColorMemory(actualResult);
 
-    // savePredictionHistory(finalPrediction, actualResult);
+    savePredictionHistory(finalPrediction, actualResult);
     
     let currentPattern = allResults.slice(1,7).join(",");
     
