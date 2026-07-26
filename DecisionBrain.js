940d7ad387298fe768148721be3f855423f7fc81
Aadi9719 <checkmeet39@gmail.com>
2026-07-26 10:00:03 +0530

    Update DecisionBrain.js

--- DecisionBrain.js
+++ DecisionBrain.js
@@ -1,1 +1,27 @@
+function getDecisionBrain() {
 
+    let patternScore = getPatternScore();
+
+    let trendScore = getTrendScore();
+
+    let aiScore = getFinalAIScore();
+
+    let bigSmallScore = getBigSmallAIScore();
+
+    let colorScore = getColorAIScore();
+
+    return {
+
+        patternScore,
+
+        trendScore,
+
+        aiScore,
+
+        bigSmallScore,
+
+        colorScore
+
+    };
+
+}
